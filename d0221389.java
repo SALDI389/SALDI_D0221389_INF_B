b1f5c81283b783ebfbbbeb8208ee6739e8623de4
SALDI_D0221389_B <99778347+SALDI389@users.noreply.github.com>
2022-02-21 11:49:35 +0800

    Add files via upload

---
+++ d0221389.java
@@ -0,0 +1,23 @@
+package inf.b;
+
+public class d0221389 {
+
+  public static void main(String[] args) {
+   
+  }
+}
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int x = scan.nextInt();
+        int d = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(d);
+    }
+}
