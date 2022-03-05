4cdc0d0e3173cefdd17a79a81741763fba83e2ff
SALDI_D0221389_B <99778347+SALDI389@users.noreply.github.com>
2022-03-05 17:26:41 +0800

    Add files via upload

---
+++ Day2_java_stdin_and_stdout.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yhummyChild;
+
+import java.util.*;
+
+public class Day2_java_stdin_and_stdout {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
+
