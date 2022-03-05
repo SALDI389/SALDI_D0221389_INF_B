add47390c6fd15f30d93e349addfa898195a0822
SALDI_D0221389_B <99778347+SALDI389@users.noreply.github.com>
2022-03-05 17:26:58 +0800

    Add files via upload

---
+++ Day3_ifelse.java
@@ -0,0 +1,36 @@
+
+package yhummyChild;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Day3_ifelse {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N%2==1){//ODD
+            System.out.println ("Weird");
+        }else{//EVEN
+            if (N>=2 && N<=5){
+                System.out.println ("Not Weird");
+            }
+            else if (6 <= N && N <= 20){
+                System.out.println ("Weird");
+            }
+            else {
+                System.out.println ("Not Weird");
+            }
+        }
+
+        scanner.close();
+    }
+}
+
